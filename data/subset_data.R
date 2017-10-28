de8d95cfb5eee2a1811a096089109b5af21fa61b
Seth Dimick <seth.dimick@nordstrom.com>
2017-10-28 15:21:07 -0700

    try to mark nulls on read.table to see if plots will be better

--- data/subset_data.R
+++ data/subset_data.R
@@ -1,8 +1,11 @@
 #Read in full txt file, subset to relevant timeperiod, write to csv
-
+data_classes <- c(rep("character", 2), rep("numeric", 7))
 full_data <- read.table("data/household_power_consumption.txt",
                         header=TRUE,
-                        sep=";")
+                        sep=";",
+                        na.strings = "?",
+                        colClasses = data_classes
+                        )
 keep_dates <- c("2/1/2007","2/2/2007")
 keep_data <- full_data[full_data$Date %in% keep_dates,]
 write.csv(keep_data,
